5ad3e1353411fd8d857fa8a4ffcb5e011bd15738
CodeMasterAR <arpitpatel343464@gmail.com>
2024-08-14 16:49:03 +0530

    commit all files

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import "./App.css";
+import MainContainer from "./components/MainContainer.jsx";
+import Login from "./components/Login.jsx";
+import { Route, Routes } from "react-router-dom";
+import Welcome from "./components/Welcome.jsx";
+import ChatArea from "./components/ChatArea.jsx";
+import Users from "./components/Users.jsx";
+import CreateGroups from "./components/CreateGroups.jsx";
+import Groups from "./components/Groups.jsx";
+import { useSelector } from "react-redux";
+
+function App() {
+  const lightTheme = useSelector((state) => state.themeKey);
+  return (
+    <div className={"App" + (lightTheme ? "" : "-dark")}>
+      {/* <MainContainer /> */}
+      {/* <Login /> */}
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="app" element={<MainContainer />}>
+          <Route path="welcome" element={<Welcome />}></Route>
+          <Route path="chat/:_id" element={<ChatArea />}></Route>
+          <Route path="users" element={<Users />}></Route>
+          <Route path="groups" element={<Groups />}></Route>
+          <Route path="create-groups" element={<CreateGroups />}></Route>
+        </Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,18 @@
+import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./Features/store.js";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
